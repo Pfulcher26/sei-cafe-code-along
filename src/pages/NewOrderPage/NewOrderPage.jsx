2d6ec13b99170baeed4d9ebd05ac607fa891c362
Jim Clark <jim.clark@generalassemb.ly>
2022-08-31 10:44:49 -0700

    Add useEffect

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+
 export default function NewOrderPage() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      setMenuItems(items);
+    }
+    getItems();
+  }, []);
+  // an empty dependency array causes the effect
+  // to run once after the first render only
+
   return (
+    <>
     <h1>NewOrderPage</h1>
+    <button onClick={() => setMenuItems(Date.now())}>Trigger re-render</button>
+    </>
   );
 }
