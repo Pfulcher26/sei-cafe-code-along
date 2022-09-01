b475d34f224c3bf80bd410a0eb0c6628035e4c96
Jim Clark <jim.clark@generalassemb.ly>
2022-09-01 10:22:31 -0700

    Add cart

--- models/order.js
+++ models/order.js
@@ -21,7 +21,8 @@ const orderSchema = new Schema({
   lineItems: [lineItemSchema],
   isPaid: { type: Boolean, default: false }
 }, {
-  timestamps: true
+  timestamps: true,
+  toJSON: { virtuals: true }
 });
 
 orderSchema.virtual('orderTotal').get(function() {
@@ -35,5 +36,18 @@ orderSchema.virtual('totalQty').get(function() {
 orderSchema.virtual('orderId').get(function() {
   return this.id.slice(-6).toUpperCase();
 });
+
+orderSchema.statics.getCart = function(userId) {
+  // 'this' is bound to the model (don't use an arrow function)
+  // return the promise that resolves to a cart (the user's unpaid order)
+  return this.findOneAndUpdate(
+    // query
+    { user: userId, isPaid: false },
+    // update - in the case the order (cart) is upserted
+    { user: userId },
+    // upsert option creates the doc if it doesn't exist!
+    { upsert: true, new: true }
+  );
+};
 
 module.exports = mongoose.model('Order', orderSchema);
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import * as itemsAPI from '../../utilities/items-api';
+import * as ordersAPI from '../../utilities/orders-api';
 import './NewOrderPage.css';
 import Logo from '../../components/Logo/Logo';
 import MenuList from '../../components/MenuList/MenuList';
@@ -11,6 +12,7 @@ import UserLogOut from '../../components/UserLogOut/UserLogOut';
 export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
   const [activeCat, setActiveCat] = useState('');
+  const [cart, setCart] = useState(null);
   const categoriesRef = useRef([]);
 
   useEffect(function() {
@@ -21,6 +23,11 @@ export default function NewOrderPage({ user, setUser }) {
       setActiveCat(categoriesRef.current[0]);
     }
     getItems();
+    async function getCart() {
+      const cart = await ordersAPI.getCart();
+      setCart(cart);
+    }
+    getCart();
   }, []);
   // an empty dependency array causes the effect
   // to run once after the first render only
