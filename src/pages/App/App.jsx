38225893f8aec57f46b714468f064d1d6ebbf702
pfulcher26 <pfulcher26@gmail.com>
2023-04-10 18:24:34 -0700

    Sets up starter code for sei cafe code along.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -12,12 +13,11 @@ export default function App() {
     <main className="App">
       { user ?
         <>
+          <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* client-side route that renders the component instance if the path matches the url in the address bar */}
-            <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
-            <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
-            {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
-            <Route path="/*" element={<Navigate to="/orders/new" />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
           </Routes>
         </>
         :
